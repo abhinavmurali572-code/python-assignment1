756c37314e0afb792fff104ff304bdee58cc3b6d
abhinavmurali572-code <abhinavmurali572@gmail.com>
2026-09-19 16:03:29 +0530

    Add files via upload

---
+++ python assignment1.py
@@ -0,0 +1,32 @@
+#list
+
+l=[1,2,3,4,5]
+print(l)
+l.insert(0,6)
+l.insert(1,7,)
+l.insert(2,8,)
+print(l)
+for i in l:
+    print(i)
+
+#dictionary
+
+mydict={"name":"john","age":25,"address":"new york"}
+print(mydict)
+mydict.update({"phone":1234567890})
+print(mydict)
+
+#set
+
+x={1,2,3,4,5}
+print(x)
+x.add(6)
+print(x)
+x.remove(3)
+print(x)
+
+#tuple
+
+a=(1,2,3,4)
+print(a)
+print(len(a))
